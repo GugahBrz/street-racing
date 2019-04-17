91ee275f6f5d4b36b1905bd292ed0895ab71732f
Gustavo <gutavato@gmail.com>
2019-04-17 18:00:19 +0200

    Add GameActivity Class

---
+++ app/src/main/java/com/zanoni/streetracing/GameActivity.java
@@ -0,0 +1,12 @@
+package com.zanoni.streetracing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/zanoni/streetracing/MainActivity.java
+++ app/src/main/java/com/zanoni/streetracing/MainActivity.java
@@ -1,13 +1,29 @@
 package com.zanoni.streetracing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Define btn_start_race
+        ImageButton btn_start_race = findViewById(R.id.btn_start_race);
+        // Set Listener
+        btn_start_race.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start Game
+                Intent it = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(it);
+            }
+        });
     }
+
 }
